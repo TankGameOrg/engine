41fca82187a6d831d297b497d6bc0aa03379948d
Trevor Brunette <trevorbrunette@gmail.com>
2024-05-06 19:24:51 -0600

    Fixed test package statement

--- src/test/java/pro/trevor/tankgame/rule/CouncilBaseIncomeTest.java
+++ src/test/java/pro/trevor/tankgame/rule/CouncilBaseIncomeTest.java
@@ -1,4 +1,4 @@
-package pro.trevor.tankgame.Rule;
+package pro.trevor.tankgame.rule;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
