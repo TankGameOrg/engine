40420f198b606016398e164358b7bac024483a99
bryanf <btrf521@gmail.com>
2024-05-18 11:21:20 -0600

    Adds new test cases making sure that units are shot before floors are

--- src/test/java/pro/trevor/tankgame/state/DestructibleFloorTest.java
+++ src/test/java/pro/trevor/tankgame/state/DestructibleFloorTest.java
@@ -8,12 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import pro.trevor.tankgame.rule.definition.ConditionalRule;
 import pro.trevor.tankgame.rule.definition.player.PlayerActionRule;
+import pro.trevor.tankgame.rule.impl.shared.rule.ConditionalRules;
 import pro.trevor.tankgame.rule.impl.shared.rule.PlayerRules;
 import pro.trevor.tankgame.rule.impl.version3.Tank;
 import pro.trevor.tankgame.state.attribute.Attribute;
 import pro.trevor.tankgame.state.board.Position;
 import pro.trevor.tankgame.state.board.floor.DestructibleFloor;
+import pro.trevor.tankgame.state.board.unit.BasicWall;
 import pro.trevor.tankgame.util.TankBuilder;
 import pro.trevor.tankgame.util.TestUtilities;
 
@@ -130,6 +133,64 @@ public class DestructibleFloorTest {
 
         // Shoot at the destructible floor, destroying it
         shootRule.apply(s, t, new Position("B1"), true);
+        assertEquals(0, Attribute.DURABILITY.unsafeFrom(floor));
+        assertTrue(Attribute.DESTROYED.from(floor).orElse(false));
+
+        // Try to move onto the broken floor, you cannot
+        assertFalse(moveRule.canApply(s, t, new Position("B1")));
+        assertThrows(Error.class, () -> moveRule.apply(s, t, new Position("B1")));
+    }
+
+    @Test
+    void shootUnitAboveFloorTest()
+    {
+        Tank t = TankBuilder.buildV3Tank().with(Attribute.ACTION_POINTS, 3).with(Attribute.DEAD, false).with(Attribute.RANGE, 2).with(Attribute.GOLD, 0).at(new Position("A1")).finish();
+        Tank tankAbove = TankBuilder.buildV3Tank().with(Attribute.DURABILITY, 1).with(Attribute.ACTION_POINTS, 3).with(Attribute.DEAD, false).with(Attribute.GOLD, 0).with(Attribute.BOUNTY, 0).at(new Position("B1")).finish();
+
+        State s = TestUtilities.generateBoard(3, 1, t, tankAbove);
+        int initialFloorDurability = 1;
+        DestructibleFloor floor = GetTestFloor(new Position("B1"), initialFloorDurability, 3);
+        s.getBoard().putFloor(floor);
+
+        PlayerActionRule<Tank> shootRule = PlayerRules.SHOOT_V4;
+        ConditionalRule<Tank> dieOrDestroyRule = ConditionalRules.GetKillOrDestroyTankOnZeroDurabilityRule();
+        shootRule.apply(s, t, new Position("B1"), true);
+        dieOrDestroyRule.apply(s, t);
+        dieOrDestroyRule.apply(s, tankAbove);
+
+        // Floor durability is unchanged
+        assertEquals(initialFloorDurability, Attribute.DURABILITY.unsafeFrom(floor));
+        assertTrue(Attribute.DEAD.unsafeFrom(tankAbove));
+        assertFalse(Attribute.DESTROYED.from(floor).orElse(false));
+    }
+
+    @Test
+    void destroyUnitAboveFloorTest()
+    {
+        Tank t = TankBuilder.buildV3Tank().with(Attribute.ACTION_POINTS, 3).with(Attribute.DEAD, false).with(Attribute.RANGE, 2).with(Attribute.GOLD, 0).at(new Position("A1")).finish();
+        BasicWall wall = new BasicWall(new Position("B1"), 1);
+
+        State s = TestUtilities.generateBoard(3, 1, t, wall);
+        int initialFloorDurability = 1;
+        DestructibleFloor floor = GetTestFloor(new Position("B1"), initialFloorDurability, 3);
+        s.getBoard().putFloor(floor);
+        
+        PlayerActionRule<Tank> shootRule = PlayerRules.SHOOT_V4;
+        ConditionalRule<BasicWall> destroyWallRule = ConditionalRules.DESTROY_WALL_ON_ZERO_DURABILITY;
+        PlayerActionRule<Tank> moveRule = PlayerRules.GetMoveRule(Attribute.ACTION_POINTS, 1);
+        
+        // Shoot once, destroying the wall
+        shootRule.apply(s, t, new Position("B1"), true);
+        destroyWallRule.apply(s, wall);
+
+        // Floor durability is unchanged
+        assertEquals(initialFloorDurability, Attribute.DURABILITY.unsafeFrom(floor));
+        assertFalse(Attribute.DESTROYED.from(floor).orElse(false));
+
+        //Shoot again
+        shootRule.apply(s, t, new Position("B1"), true);
+
+        // Floor is destroyed
         assertEquals(0, Attribute.DURABILITY.unsafeFrom(floor));
         assertTrue(Attribute.DESTROYED.from(floor).orElse(false));
 
