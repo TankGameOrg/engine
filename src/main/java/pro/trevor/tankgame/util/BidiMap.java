3f989dd7356d4d253e266cc090b62e94fe70ab3c
Ryan <ryan314r@gmail.com>
2024-06-20 21:47:58 -0600

    Read only Bidirectiontal HashMap (#69)
    * Read only Bidirectiontal HashMap
    
    ---------
    
    Co-authored-by: Trevor Brunette <trevorbrunette@gmail.com>

---
+++ src/main/java/pro/trevor/tankgame/util/BidiMap.java
@@ -0,0 +1,38 @@
+package pro.trevor.tankgame.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BidiMap<K, V> {
+    private final Map<K, V> keyToValue;
+    private final Map<V, K> valueToKey;
+
+    public BidiMap() {
+        keyToValue = new HashMap<>();
+        valueToKey = new HashMap<>();
+    }
+
+    public BidiMap(List<Pair<K, V>> pairs) {
+        keyToValue = new HashMap<>();
+        valueToKey = new HashMap<>();
+        for(Pair<K, V> pair : pairs) {
+            put(pair.left(), pair.right());
+        }
+    }
+
+    void put(K key, V value) {
+        assert !keyToValue.containsKey(key);
+        assert !valueToKey.containsKey(value);
+        keyToValue.put(key, value);
+        valueToKey.put(value, key);
+    }
+
+    V getValue(K key) {
+        return keyToValue.get(key);
+    }
+
+    K getKey(V value) {
+        return valueToKey.get(value);
+    }
+}
