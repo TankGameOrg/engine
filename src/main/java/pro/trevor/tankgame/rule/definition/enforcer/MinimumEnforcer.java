bfd32ba718dea174dca4b116ac1dfa9ad49ed82b
Ryan Ray <ryan314r@gmail.com>
2024-08-04 17:10:11 -0600

    Use attributes for min/max enforcers

--- src/main/java/pro/trevor/tankgame/rule/definition/enforcer/MinimumEnforcer.java
+++ src/main/java/pro/trevor/tankgame/rule/definition/enforcer/MinimumEnforcer.java
@@ -6,6 +6,20 @@ import pro.trevor.tankgame.state.attribute.AttributeObject;
 public class MinimumEnforcer<T extends AttributeObject, U extends Comparable<U>> extends AttributePredicateEnforcer<T, U> {
 
     public MinimumEnforcer(Attribute<U> attribute, U bound) {
-        super((x) -> attribute.in(x) && attribute.unsafeFrom(x).compareTo(bound) > 0, attribute, bound);
+        super((x) -> attribute.in(x) && attribute.unsafeFrom(x).compareTo(bound) > 0, attribute, (t) -> bound);
+    }
+
+    public MinimumEnforcer(Attribute<U> attribute, Attribute<U> boundAttribute, U defaultBound) {
+        super(
+            (x) -> {
+                if(attribute.in(x)) {
+                    U bound = boundAttribute.fromOrElse(x, defaultBound);
+                    return attribute.unsafeFrom(x).compareTo(bound) > 0;
+                }
+
+                return false;
+            },
+            attribute,
+            (x) -> boundAttribute.fromOrElse(x, defaultBound));
     }
 }
