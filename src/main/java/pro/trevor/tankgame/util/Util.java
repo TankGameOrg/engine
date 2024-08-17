639445dca3491b31dbbe74fa8d3486d21e68b0d7
Trevor Brunette <trevorbrunette@gmail.com>
2024-08-17 16:20:06 -0600

    Knowledge of paths

--- src/main/java/pro/trevor/tankgame/util/Util.java
+++ src/main/java/pro/trevor/tankgame/util/Util.java
@@ -129,30 +129,57 @@ public class Util {
         return output;
     }
 
-    public static Set<Position> possibleMoves(State state, Position p, int speed) {
-        HashMap<Position, Integer> output = new HashMap<>();
-        possibleMovesHelper(output, state, p, speed);
-        return output.keySet();
+    public static Set<Position> allPossibleMoves(Board board, Position p, int speed) {
+        return allPossiblePaths(board, p, speed).keySet();
     }
 
-    private static void possibleMovesHelper(HashMap<Position, Integer> visited, State state, Position p, int speed) {
-        if (visited.getOrDefault(p, -1) >= speed || (!state.getBoard().isWalkable(p) && !visited.isEmpty())) {
+    /**
+     * Finds the shortest path to each space within the unit's speed. The result of this is stored into the return
+     * value as a hashmap of Position(Target) -> Pair[Integer(Remaining speed), Position(Previous position)].
+     * Ties in optimal paths are arbitrarily broken.
+     * @param board The board to check for if positions are valid.
+     * @param p The position to check for paths from.
+     * @param speed The number of spaces to search. Must be non-negative.
+     */
+    public static HashMap<Position, Pair<Integer, Position>> allPossiblePaths(Board board, Position p, int speed) {
+        HashMap<Position, Pair<Integer, Position>> output = new HashMap<>();
+        allPossiblePaths(output, board, p, null, speed);
+        return output;
+    }
+
+    /**
+     * Finds the shortest path to each space within the unit's speed. This information is stored into the first
+     * parameter as a hashmap of Position(Target) -> Pair[Integer(Remaining speed), Position(Previous position)].
+     * Ties in optimal paths are arbitrarily broken.
+     * @param visited The output parameter. Must be empty.
+     * @param board The board to check for if positions are valid.
+     * @param p The position to check for paths from.
+     * @param prev The previous position. Must be null.
+     * @param speed The number of spaces to search. Must be non-negative.
+     */
+    private static void allPossiblePaths(HashMap<Position, Pair<Integer, Position>> visited, Board board, Position p, Position prev, int speed) {
+        // If the map of visited positions contains this with an equal or higher speed, return.
+        // If the map of visited positions is empty, this is our first node. It might not be walkable since it could be
+        // the space that a tank is already occupying.
+        if (visited.getOrDefault(p, Pair.of(-1, null)).left() >= speed || (!visited.isEmpty() && !board.isWalkable(p))) {
             return;
         }
 
-        visited.put(p, speed);
+        visited.put(p, Pair.of(speed, prev));
 
         if (speed == 0) {
             return;
         }
 
-        for (Position position : allAdjacentMovablePositions(state.getBoard(), p)) {
-            possibleMovesHelper(visited, state, position, speed - 1);
+        // Visit all valid adjacent spaces with a decreased number of remaining moves.
+        // Any previously-visited spaces with greater speed values will be ignored.
+        for (Position position : allAdjacentMovablePositions(board, p)) {
+            allPossiblePaths(visited, board, position, p,speed - 1);
         }
     }
 
     public static boolean canMoveTo(State state, Position s, Position e, int speed) {
-        Set<Position> possibleMoves = possibleMoves(state, s, speed);
+        Set<Position> possibleMoves = allPossibleMoves(state.getBoard(), s, speed);
         return possibleMoves.contains(e);
     }
 
